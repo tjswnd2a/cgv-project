932e0bca20288d6c1fe868efbe04adfc1379c4fc
tjswnd2a <tjswnd2a@naver.com>
2022-09-06 18:01:24 +0900

    css final

--- js/swiper.js
+++ js/swiper.js
@@ -47,3 +47,23 @@ export function autoplay_controller(auto) {
     event_swiper.autoplay.stop();
   }
 }
+
+var card_swiper = new Swiper('.cgv-info .swiper', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
